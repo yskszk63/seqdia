5d86a2a1d8e036dd84b9ce0576211a833bec787e
yusuke suzuki <yskszk63@gmail.com>
2020-09-05 11:56:21 +0900

    Use wasm pack (#8)
    * use_wasm_pack
    
    * fix build
    
    * fix_webconfig
    
    * test_deploy
    
    * test_deploy
    
    * test_deploy
    
    * use prebuild
    
    * fix
    
    * fixpath
    
    * fixpath
    
    * fixpath
    
    * use peaceiris/actions-gh-pages@v3

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const path = require("path");
+
+const docs = path.resolve(__dirname, "docs");
+
+module.exports = {
+  mode: "production",
+  entry: {
+      index: "./bootstrap.js",
+  },
+  output: {
+    path: docs,
+    filename: "[name].js",
+  },
+  devServer: {
+    contentBase: docs,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin(['index.html']),
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ],
+};
